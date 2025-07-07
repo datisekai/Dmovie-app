6f96ce50e377adea0f03891055665080d8161825
Datisekai <datly030102@gmail.com>
2025-07-07 17:54:41 +0700

    feat: add qc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { onAuthStateChanged } from "firebase/auth";
 import type { AppProps } from "next/app";
 import NextNProgress from "nextjs-progressbar";
 import { Provider } from "react-redux";
+import Head from "next/head";
+import { useEffect } from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -10,10 +12,41 @@ import ThemeLayout from "../src/components/layout/ThemeLayout";
 import { auth } from "../src/config/firebase";
 import store from "../src/redux/store";
 import "../styles/globals.css";
+import { useRouter } from "next/router";
+
+// Declare AdProvider type
+declare global {
+  interface Window {
+    AdProvider?: any[];
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    // Initialize AdProvider after the script is loaded
+    const initAdProvider = () => {
+      if (typeof window !== "undefined" && window.AdProvider) {
+        (window.AdProvider = window.AdProvider || []).push({ serve: {} });
+      } else {
+        // Retry after a short delay if AdProvider is not yet available
+        setTimeout(initAdProvider, 100);
+      }
+    };
+
+    // Start initialization after component mounts
+    initAdProvider();
+  }, [router.asPath]);
+
   return (
     <>
+      <Head>
+        <script
+          async
+          type="application/javascript"
+          src="https://a.magsrv.com/ad-provider.js"
+        ></script>
+      </Head>
       <NextNProgress
         showOnShallow={true}
         options={{
@@ -23,6 +56,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Provider store={store}>
         <ThemeLayout>
           <Component {...pageProps} />
+          <div style={{ margin: "20px 0", textAlign: "center" }}>
+            <ins className="eas6a97888e6" data-zoneid="5668188"></ins>
+          </div>
         </ThemeLayout>
       </Provider>
     </>
